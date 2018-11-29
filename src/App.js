657529ec288466c81487cc5c2a85d56b206084d3
Konecki <koneckit@emea.roche.com>
2018-11-29 16:23:57 +0100

    Add DatePicker

--- src/App.js
+++ src/App.js
@@ -1,61 +1,28 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
 import "./scss/App.scss";
-// import Filter from "./components/Filter";
-import people from "./data/people";
-
-// function searchingFor(term) {
-//   return function(x) {
-//     return x.first.toLowerCase().includes(term.toLowerCase()) || !term;
-//   };
-// }
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      people,
-      term: "",
-      description: false
+      startDate: new Date()
     };
   }
 
-  setFlag = value => {
-    console.log(value);
-    let flag = this.state.description;
-    this.setState({
-      description: flag ? false : true
-    });
-  };
-
-  searchingFor = term => x =>
-    x.first.toLowerCase().includes(term.toLowerCase()) ||
-    x.last.toLowerCase().includes(term.toLowerCase());
-
-  searchHandler = e => {
-    this.setState({
-      term: e.target.value
-    });
+  handleChange = e => {
+    console.log("handleChange invoked...", e);
   };
 
   render() {
-    const { term, people, description } = this.state;
     return (
       <div className="App">
-        <input type="text" onChange={this.searchHandler} />
-
-        {people.filter(this.searchingFor(term)).map(person => (
-          <div
-            className="person"
-            key={person.id}
-            onClick={() => this.setFlag(person.id)}
-          >
-            <h6>{person.first}</h6>
-            <h6>{person.last}</h6>
-            <h6>{person.age}</h6>
-            {description && <p className="description">Lorem ipsum</p>}
-          </div>
-        ))}
+        <h1>React Developer Test</h1>
+        <DatePicker
+          inline
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
